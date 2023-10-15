693cb1e7cce00bd970f00cc4f26cfb95933a15f6
lenincarlen <leninmejiacarlen@gmail.com>
2023-10-15 17:19:34 -0400

    first commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,8 +10,8 @@ export function Header() {
             <motion.div variants={fadeIn('up', 0.5)} initial="hidden" animate="show" exit="hidden">
                 <div className="container justify-between mx-auto md:flex">
                     <Link href="/">
-                        <h1 className="my-3 text-4xl text-center text-white md:text-lef">LeninCarlen
-                            <span className="text-secondary">/Dev</span>
+                        <h1 className="my-3 text-4xl text-center text-white md:text-lef">car
+                            <span className="text-secondary">len</span>
                         </h1>
                     </Link>
                     <div className="flex items-center justify-center gap-7">
